c1a4642feeec0f59d5fff12e1618588997f536e5
Alexandre Pereira <alexandre@placecode.com.br>
2022-03-16 13:54:58 -0300

    Config Redis

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,12 +1,17 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
+import RedisCache from '@shared/cache/RedisCache';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
 
+    const redisCache = new RedisCache();
+
     const products = await productsRepository.find();
+
+    await redisCache.save('new-teste', 'Testando cache novamente!');
 
     return products;
   }
--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -1,1 +1,19 @@
-import Redis, { RedisCli} from 'redis';
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+
+export default class RedisCache {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: string): Promise<void> {
+    console.log(key, value);
+    //await this.client.set(key, JSON.stringify(value));
+  }
+
+  //public async recover<T>(key: string): Promise<T | null> {}
+
+  //public async invalidate(key: string): Promise<void> {}
+}
